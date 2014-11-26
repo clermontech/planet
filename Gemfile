a13d3cc1bd7e4482121cd4489dc8a6e2dd212dea
William Durand <william.durand1@gmail.com>
2014-11-26 16:07:21 +0100

    Add font-awesome, compass-validator gems

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ gem 'github-pages'
 gem "sass", "~> 3.3.0"
 gem 'compass', '~> 1.0'
 gem 'foundation', '~> 1.0'
+gem 'compass-validator', '~> 3.0.0'
+gem 'font-awesome-sass', '~> 4.2.0'
--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
 
+require 'font-awesome-sass'
+
 # Set this to the root of your project when deployed:
 http_path       = "/"
 css_dir         = "css"
